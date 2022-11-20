480be765d0dd411ea53259cefd1a7b6702534919
Robertm339 <71312299+Robertm339@users.noreply.github.com>
2022-11-20 11:41:30 -0800

    Initial Commit

---
+++ SumThing/SumThingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SumThingApp.swift
+//  SumThing
+//
+//  Created by Robert Martinez on 11/20/22.
+//
+
+import SwiftUI
+
+@main
+struct SumThingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
